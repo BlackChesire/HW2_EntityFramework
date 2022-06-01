174bfb5974a814b87a9213f34210370a9616cc2e
Asaf Ben Shabat <68661307+BlackChesire@users.noreply.github.com>
2022-06-01 19:45:33 +0300

    Add project files.

---
+++ Colors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_EntityFramework
+{
+    public class Colors
+    {
+        enum Color
+        {
+            white,
+            black,
+            red,
+            green,
+            
+        }
+    }
+}
---
+++ DataModels/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_EntityFramework.DataModels
+{
+    public class Point
+    {
+        //props id,x,y, enum cololr, tav, title 
+        public Guid id { get; set; }
+        public int x { get; set; }
+        
+        public int y { get; set; }
+        
+  
+         
+    }
+}
